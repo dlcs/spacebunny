fcf9325bed9df71f5d2989de67ea14005fbab9c4
John <john-root@users.noreply.github.com>
2016-09-16 15:33:28 +0100

    added example settings file

---
+++ settings_example.py
@@ -0,0 +1,10 @@
+
+MESSAGES_PER_FETCH = 5
+POLL_INTERVAL = 20
+
+REGION = 'eu-west-1'
+INPUT_QUEUE = 'CHANGEME-default-bunny-input-queue'
+NOTIFICATION_QUEUE = 'CHANGEME-default-bunny-notification-queue'
+RESPONSE_QUEUE = 'CHANGEME-default-bunny-response-queue'
+PIPELINE = 'CHANGEME-default-bunny-pipeline'
+OUTPUT_BUCKET = 'CHANGEME-default-bunny-output-bucket'  # should be the bucket the pipeline above is configured to use
