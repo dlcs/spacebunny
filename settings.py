21d9858b3220888754513456da1a429b6ac94317
John <john-root@users.noreply.github.com>
2016-09-22 09:54:39 +0100

    added settings file as no longer contains secrets
    remove example settings
    updated gitignore

---
+++ settings.py
@@ -0,0 +1,11 @@
+import os
+
+MESSAGES_PER_FETCH = 5
+POLL_INTERVAL = 20
+
+REGION = os.environ.get('BUNNY_AWS_REGION')  # e.g. 'eu-west-1'
+INPUT_QUEUE = os.environ.get('BUNNY_INPUT_QUEUE')  # e.g. 'bunny-input'
+NOTIFICATION_QUEUE = os.environ.get('BUNNY_NOTIFICATION_QUEUE')  # e.g. 'bunny-notification'
+RESPONSE_QUEUE = os.environ.get('BUNNY_RESPONSE_QUEUE')  # e.g. 'bunny-response'
+PIPELINE = os.environ.get('BUNNY_PIPELINE')  # e.g. bunny-pipeline'
+OUTPUT_BUCKET = os.environ.get('BUNNY_OUTPUT_BUCKET')  # e.g. 'bunny-output'
