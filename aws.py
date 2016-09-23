54c03b18afe94db85dbd008c5a0e82f54d4040b2
John <john-root@users.noreply.github.com>
2016-09-23 12:34:12 +0100

    try/except around delete

--- aws.py
+++ aws.py
@@ -1,4 +1,6 @@
 import boto3
+from boto.exception import BotoServerError
+
 import settings
 import logging
 
@@ -48,7 +50,10 @@ def get_preset_map(transcoder, inverse=False):
 def delete_s3_object(s3, bucket, key):
 
     logging.debug("Attempting to delete key %s from bucket %s" % (key, bucket))
-    s3.meta.client.delete_object(Bucket=bucket, Key=key)
+    try:
+        s3.meta.client.delete_object(Bucket=bucket, Key=key)
+    except BotoServerError:
+        logging.debug("Caught exception confirming temp file %s didn't already exist in bucket %s" % (key, bucket))
 
 
 def move_s3_object(s3, bucket, old, new):
